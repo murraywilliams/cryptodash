16c2ad57005940dc3c30d891b769f12601a5eefa
Murrayw <williams.murray@gmail.com>
2019-02-07 09:11:06 +0200

    Added styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
+import WelcomeMessage from './WelcomeMessage';
+
+const MyButton = styled.div`
+  color: green;
+`;
 
 class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <WelcomeMessage />;
+      </div>
+    );
   }
 }
 
