69aed6cb4b04212dd4354c5dd025d0cc626d8bde
Murrayw <williams.murray@gmail.com>
2019-02-08 10:20:43 +0200

    Added CoiSpotlight functionality

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Page from '../Shared/Page';
 import PriceGrid from './PriceGrid';
+import CoinSpotlight from './CoinSpotlight';
+import styled from 'styled-components';
+
+const ChartGrid = styled.div`
+  display: grid;
+  margin-top: 20px;
+  grid-gap: 15px;
+  grid-template-columns: 1fr 3fr;
+`;
 
 export default () => {
   return (
     <Page name='dashboard'>
       <PriceGrid />
+      <ChartGrid>
+        <CoinSpotlight />
+        <div>Chart goes here</div>
+      </ChartGrid>
     </Page>
   );
 };
