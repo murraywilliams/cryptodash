67befbc36d0d8c55d955fb0940d13d7321066ff4
Murrayw <williams.murray@gmail.com>
2019-02-08 10:35:20 +0200

    Added highcharts

---
+++ src/Dashboard/PriceChart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Tile } from '../Shared/Tile';
+import { AppContext } from '../App/AppProvider';
+import ReactHighcharts from 'react-highcharts';
+import highchartsConfig from './HighchartsConfig';
+
+export default () => {
+  return (
+    <AppContext.Consumer>
+      {({}) => (
+        <Tile>
+          <ReactHighcharts config={highchartsConfig()} />
+        </Tile>
+      )}
+    </AppContext.Consumer>
+  );
+};
--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -3,6 +3,7 @@ import Page from '../Shared/Page';
 import PriceGrid from './PriceGrid';
 import CoinSpotlight from './CoinSpotlight';
 import styled from 'styled-components';
+import PriceChart from './PriceChart';
 
 const ChartGrid = styled.div`
   display: grid;
@@ -17,7 +18,7 @@ export default () => {
       <PriceGrid />
       <ChartGrid>
         <CoinSpotlight />
-        <div>Chart goes here</div>
+        <PriceChart />
       </ChartGrid>
     </Page>
   );
