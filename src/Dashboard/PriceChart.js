34803371697c586ff59ea4fabd807f9b5f1e02e0
Murrayw <williams.murray@gmail.com>
2019-02-08 10:46:26 +0200

    Highcharts theme

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -3,6 +3,8 @@ import { Tile } from '../Shared/Tile';
 import { AppContext } from '../App/AppProvider';
 import ReactHighcharts from 'react-highcharts';
 import highchartsConfig from './HighchartsConfig';
+import highchartsTheme from './HightchartsTheme';
+ReactHighcharts.Highcharts.setOptions(highchartsTheme);
 
 export default () => {
   return (
